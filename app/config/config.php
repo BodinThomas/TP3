62fdf592cd09f0ac207df4e4239a182c704601ac
Thomas Bodin <21600286@campus.unicaen.fr>
2018-05-18 13:42:12 +0200

    First commit

--- app/config/config.php
+++ app/config/config.php
@@ -1,42 +1,30 @@
 <?php
 return array(
-	"siteUrl"=>"http://127.0.0.1/TP3/",
-	"database"=>array(
-			"type"=>"mysql",
-			"dbName"=>"messagerie",
-			"serverName"=>"127.0.0.1",
-			"port"=>3306,
-			"user"=>"root",
-			"password"=>"",
-			"options"=>array(),
-			"cache"=>false
-			),
-	"sessionName"=>"TP3",
-	"namespaces"=>array(),
-	"templateEngine"=>"Ubiquity\\views\\engine\\Twig",
-	"templateEngineOptions"=>array(
-			"cache"=>false
-			),
-	"test"=>false,
-	"debug"=>false,
-	"di"=>array(
-			"jquery"=>function ($controller){
+		"siteUrl"=>"http://127.0.0.1/tp3/",
+		"database"=>[
+				"type"=>"mysql",
+				"dbName"=>"",
+				"serverName"=>"127.0.0.1",
+				"port"=>"3306",
+				"user"=>"root",
+				"password"=>"",
+				"options"=>[],
+				"cache"=>false
+		],
+		"sessionName"=>"tp3",
+		"namespaces"=>[],
+		"templateEngine"=>'Ubiquity\\views\\engine\\Twig',
+		"templateEngineOptions"=>array("cache"=>false),
+		"test"=>false,
+		"debug"=>false,
+		"di"=>["jquery"=>function($controller){
 							$jquery=new \Ajax\php\ubiquity\JsUtils(["defer"=>true],$controller);
 							$jquery->semantic(new \Ajax\Semantic());
 							return $jquery;
-						}
-			),
-	"cache"=>array(
-			"directory"=>"cache/",
-			"system"=>"Ubiquity\\cache\\system\\ArrayCache",
-			"params"=>array()
-			),
-	"mvcNS"=>array(
-			"models"=>"models",
-			"controllers"=>"controllers",
-			"rest"=>""
-			),
-	"isRest"=>function (){
+						}],
+		"cache"=>["directory"=>"cache/","system"=>"Ubiquity\\cache\\system\\ArrayCache","params"=>[]],
+		"mvcNS"=>["models"=>"models","controllers"=>"controllers","rest"=>""],
+		"isRest"=>function(){
 			return \Ubiquity\utils\http\URequest::getUrlParts()[0]==="rest";
 		}
-	);
+);
